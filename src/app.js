3375a380c418d5718652a3ee4946a25ffea25b56
yun <loverglory233@gmail.com>
2026-08-24 07:49:03 +0800

    fix: 自动修改宝塔SQLite站点属性将静态升级为PHP模式(v187)

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260823_v186";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired } from "./utils.js?v=20260823_v186";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v186";
-import { AuthManager } from "./auth.js?v=20260823_v186";
-import { CloudSyncEngine } from "./sync.js?v=20260823_v186";
-import { renderLoginView } from "./login.js?v=20260823_v186";
-import { renderTeacherPortal } from "./teacher.js?v=20260823_v186";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260823_v186";
+} from "./constants.js?v=20260823_v187";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired } from "./utils.js?v=20260823_v187";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v187";
+import { AuthManager } from "./auth.js?v=20260823_v187";
+import { CloudSyncEngine } from "./sync.js?v=20260823_v187";
+import { renderLoginView } from "./login.js?v=20260823_v187";
+import { renderTeacherPortal } from "./teacher.js?v=20260823_v187";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260823_v187";
 import {
   buildWordEditorHtml,
   attachWordEditorEvents,
@@ -26,7 +26,7 @@ import {
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260823_v186";
+} from "./editor.js?v=20260823_v187";
 
 // Make renderChat available on window for sync callbacks
 if (typeof window !== "undefined") {
