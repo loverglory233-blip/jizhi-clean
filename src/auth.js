7eb72dd7add079a062118eb89d43369eabf6470b
yun <loverglory233@gmail.com>
2026-08-24 07:09:58 +0800

    fix: 彻底剔除默认数据覆盖逻辑+支持从独立关系表聚合还原所有真实任务与学生(v158)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v157';
-import { formatExportDateTime } from './utils.js?v=20260823_v157';
+} from './constants.js?v=20260823_v158';
+import { formatExportDateTime } from './utils.js?v=20260823_v158';
 
 export class AuthManager {
   constructor() {
