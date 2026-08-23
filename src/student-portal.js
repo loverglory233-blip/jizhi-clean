9f7f6e12cb8342c285480aa0dc8f9598f4bd4541
yun <loverglory233@gmail.com>
2026-08-24 04:07:54 +0800

    feat: 科学限定研讨深度判定指标+确保智能体全量公布投票并深度点评+升级v133

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v132";
-import { escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v132";
+} from "./constants.js?v=20260823_v133";
+import { escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v133";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
--- src/teacher.js
+++ src/teacher.js
@@ -9,8 +9,8 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260823_v132";
-import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v132";
+} from "./constants.js?v=20260823_v133";
+import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v133";
 
 /* ==========================================================================
    7. TEACHER PORTAL RENDERER (LIVE WORKSPACE MIRROR & ANNOUNCEMENT READ MATRIX)
