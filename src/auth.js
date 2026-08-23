20ea5dbd3cac46b7c9cd986a6e10cba2229c6dc9
yun <loverglory233@gmail.com>
2026-08-24 07:30:34 +0800

    fix: 彻底删除阶段一提案中画蛇添足的AI评估冗余字段保持与学生端纯净一致(v171)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v170';
-import { formatExportDateTime } from './utils.js?v=20260823_v170';
+} from './constants.js?v=20260823_v171';
+import { formatExportDateTime } from './utils.js?v=20260823_v171';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v170';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v170';
+import { InitialState } from './constants.js?v=20260823_v171';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v171';
 
 export class CloudSyncEngine {
   constructor(app) {
