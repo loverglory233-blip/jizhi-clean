14052647e08e8016bf7813ccba3d8cfd6e55a978
yun <loverglory233@gmail.com>
2026-08-23 16:32:42 +0800

    fix: 修复app.js中的try语法嵌套问题+升级v90

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v89';
+} from './constants.js?v=20260823_v90';
 
 export class AuthManager {
   constructor() {
