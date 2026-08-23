2909eb56985ba72cafd1ebc659aac9119434437c
yun <loverglory233@gmail.com>
2026-08-23 16:36:36 +0800

    fix: Etherpad采用标准同源反向代理路径/etherpad/解决HTTPS证书与混合内容阻断+升级v92

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v91';
+} from './constants.js?v=20260823_v92';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v91';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v91';
+import { InitialState } from './constants.js?v=20260823_v92';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v92';
 
 export class CloudSyncEngine {
   constructor(app) {
