485c2f0b1f5f9bb673380225f32d3ff78483e53b
yun <loverglory233@gmail.com>
2026-08-23 12:15:44 +0800

    fix: 修复修改密码账号显示教师工号 + 彻底解决修改密码报账号不存在的问题 + 升级v20

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v19';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v19';
+import { InitialState } from './constants.js?v=20260823_v20';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v20';
 
 export class CloudSyncEngine {
   constructor(app) {
