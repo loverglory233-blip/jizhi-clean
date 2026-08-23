2721d9750c4fb013f1e89475ecff6c24e10d314d
yun <loverglory233@gmail.com>
2026-08-24 03:50:19 +0800

    fix: 重构聊天头部两行独立布局+修复合约一键生成草案与重新渲染+升级v126

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260823_v125";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v125";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v125";
+import { AgentProfiles } from "./constants.js?v=20260823_v126";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v126";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v126";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v125';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v125';
+import { InitialState } from './constants.js?v=20260823_v126';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v126';
 
 export class CloudSyncEngine {
   constructor(app) {
