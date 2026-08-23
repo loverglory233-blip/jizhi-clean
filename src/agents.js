29c0c24680c95e83d032eaf362c299476c263807
yun <loverglory233@gmail.com>
2026-08-24 01:23:41 +0800

    feat: 提供check_etherpad_plugins.sh插件自检诊断工具+升级v110

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v109';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v110';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey];
