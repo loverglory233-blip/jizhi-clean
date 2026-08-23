534a6f36812fbc784372bd463d9b8cc0755455d3
yun <loverglory233@gmail.com>
2026-08-24 04:38:12 +0800

    release: 升级至20260823_v137+全量编译打包bundle.js+Express插件静态路由终极注入

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v136';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v137';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey];
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v136';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v136';
+import { InitialState } from './constants.js?v=20260823_v137';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v137';
 
 export class CloudSyncEngine {
   constructor(app) {
