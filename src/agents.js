c960a240348bf9f1382f488b7bf3b4f9f6d21430
yun <loverglory233@gmail.com>
2026-08-23 12:53:45 +0800

    fix: 彻底打通教师创建/导入学生即时同步入库与登录自愈+恢复高灵敏顶号拦截下线+升级v35

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v34';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v35';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey];
