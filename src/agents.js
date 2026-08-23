59f84c44419a3f076b0963dabbbae6e7e34a2270
yun <loverglory233@gmail.com>
2026-08-24 06:27:57 +0800

    fix: 彻底移除遮罩层+提供一键Etherpad后台极速自愈就绪脚本(v144)

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v143';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v144';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey];
