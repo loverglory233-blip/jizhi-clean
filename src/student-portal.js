ccf7c0c838963b2a724361b1c84d93f8fe1edf2d
yun <loverglory233@gmail.com>
2026-08-23 12:07:57 +0800

    feat: 规范任务默认时间(开始=当前, 截止>=当前+2h) + 任务截止全阶段只读且支持自由切换查阅(聊天区保留打字) + 升级v15

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v14";
-import { escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v14";
+} from "./constants.js?v=20260823_v15";
+import { escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v15";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
