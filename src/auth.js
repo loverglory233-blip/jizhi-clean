451541d67194d4741d3cd5e220f4f56a14b9c549
yun <loverglory233@gmail.com>
2026-08-23 15:08:39 +0800

    fix: 彻底拔除外层contenteditable解除Quill事件劫持+纯单轨Yjs满血打通+升级v78

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v77';
+} from './constants.js?v=20260823_v78';
 
 export class AuthManager {
   constructor() {
