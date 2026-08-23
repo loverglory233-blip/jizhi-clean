e45798c23553e65622dc5ceb3a7aa48cf7313545
yun <loverglory233@gmail.com>
2026-08-24 03:57:28 +0800

    feat: 为公约提炼添加严格研讨门禁校验防止未讨论前生成假模板+升级v128

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v127';
+} from './constants.js?v=20260823_v128';
 
 export class AuthManager {
   constructor() {
