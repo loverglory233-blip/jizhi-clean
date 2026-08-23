aa88309eecff597b7d038e34d032c49a938122e2
yun <loverglory233@gmail.com>
2026-08-23 11:42:30 +0800

    chore: 全量升级版本戳 v10 -> v11，强制浏览器重新拉取包含 autoRandomGrouping 的最新代码

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v10';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v10';
+import { InitialState } from './constants.js?v=20260823_v11';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v11';
 
 export class CloudSyncEngine {
   constructor(app) {
