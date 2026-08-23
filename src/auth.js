ddc669de4df949aadf5b73f440f4192416371a78
yun <loverglory233@gmail.com>
2026-08-24 05:59:06 +0800

    release: 升级至20260823_v138最新版+编译打包bundle.js+极简稳定启动脚本

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v137';
+} from './constants.js?v=20260823_v138';
 
 export class AuthManager {
   constructor() {
