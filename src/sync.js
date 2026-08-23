a56ddcbd0bcadd095591ce5f0f6b1d08fd89e0cf
yun <loverglory233@gmail.com>
2026-08-23 16:29:12 +0800

    feat: 落地顶级非阻塞异步静默投递+动态推敲气泡彻底解决干等与反复重试焦虑+升级v89

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v88';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v88';
+import { InitialState } from './constants.js?v=20260823_v89';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v89';
 
 export class CloudSyncEngine {
   constructor(app) {
