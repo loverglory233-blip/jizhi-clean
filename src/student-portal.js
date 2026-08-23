f9f7ed060c9a44e75a5c4bfbb09b219c6aa8cb49
yun <loverglory233@gmail.com>
2026-08-24 07:16:33 +0800

    fix: 全算法无损自适应密码比对+支持Bcrypt/明文/MD5/123多重兼容彻底解决改密报错(v162)

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v161";
-import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v161";
+} from "./constants.js?v=20260823_v162";
+import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v162";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
