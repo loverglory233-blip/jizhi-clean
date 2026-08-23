04c78f47b6a91595a7227c66d709b31a39906a16
yun <loverglory233@gmail.com>
2026-08-24 06:44:50 +0800

    fix: 全链路地毯式审查并加固全要素实时协同同步引擎(v152)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260823_v151";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v151";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v151";
+import { AgentProfiles } from "./constants.js?v=20260823_v152";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v152";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v152";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v151';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v151';
+import { InitialState } from './constants.js?v=20260823_v152';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v152';
 
 export class CloudSyncEngine {
   constructor(app) {
