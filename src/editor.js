bafcb7e60701ebe9f2a600483ddc92f9d4feb4b5
yun <loverglory233@gmail.com>
2026-08-24 07:38:24 +0800

    fix: 回归最标准严谨的纯明文密码比对与原密码校验(v178)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260823_v177";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v177";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v177";
+import { AgentProfiles } from "./constants.js?v=20260823_v178";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v178";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v178";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
