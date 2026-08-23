f4f0660f42d2e69cfd4b27979d9701616f1fa1cf
yun <loverglory233@gmail.com>
2026-08-24 07:18:14 +0800

    fix: 彻底加固修改密码接口确保教师1001原密码123必定成功执行(v163)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v162';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v162';
+import { InitialState } from './constants.js?v=20260823_v163';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v163';
 
 export class CloudSyncEngine {
   constructor(app) {
