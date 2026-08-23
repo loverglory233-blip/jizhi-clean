8b183af7c9ac4aa956d4b7ff0fb21856e89f5e6a
yun <loverglory233@gmail.com>
2026-08-24 04:14:00 +0800

    feat: 严格落实仅全票一致才算定题+非全票一律按分歧引导研讨+升级v136

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v135';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v135';
+import { InitialState } from './constants.js?v=20260823_v136';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v136';
 
 export class CloudSyncEngine {
   constructor(app) {
