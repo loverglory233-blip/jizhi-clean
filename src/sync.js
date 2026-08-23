04ebe6383b97626155de44f15c1fe5b5a0489ea6
yun <loverglory233@gmail.com>
2026-08-23 14:17:40 +0800

    fix: 彻底剔除QuillBinding实例化后的破坏性pasteHTML确保Yjs双向微秒协同100%生效+升级v67

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v66';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v66';
+import { InitialState } from './constants.js?v=20260823_v67';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v67';
 
 export class CloudSyncEngine {
   constructor(app) {
