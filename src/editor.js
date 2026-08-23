893f9178d4893a97de1e66328ce9df24e4b8b30b
yun <loverglory233@gmail.com>
2026-08-23 12:44:09 +0800

    fix: 修复teacher.js闭合语法并升级v33

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260823_v32";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v32";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v32";
+import { AgentProfiles } from "./constants.js?v=20260823_v33";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v33";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v33";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v32';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v32';
+import { InitialState } from './constants.js?v=20260823_v33';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v33';
 
 export class CloudSyncEngine {
   constructor(app) {
