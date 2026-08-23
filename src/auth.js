2ef2c4cec2f85014e878bbc63cb0d7948cb3ac47
yun <loverglory233@gmail.com>
2026-08-24 02:21:28 +0800

    fix: 建立src/node_modules软链接彻底解决Settings.js模块寻找不到问题+升级v118

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,7 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v117';
+} from './constants.js?v=20260823_v118';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v117';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v117';
+import { InitialState } from './constants.js?v=20260823_v118';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v118';
 
 export class CloudSyncEngine {
   constructor(app) {
