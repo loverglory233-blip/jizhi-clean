d4fdf6a6f94281b6c47faa8f416f7d15e55e6cbf
yun <loverglory233@gmail.com>
2026-08-23 12:55:14 +0800

    fix: 重构通知确认闭环流转+全链路畅通状态机与数据双向同步+升级v36

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v35';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v35';
+import { InitialState } from './constants.js?v=20260823_v36';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v36';
 
 export class CloudSyncEngine {
   constructor(app) {
