af36e43e362c9ff88cb60ca187a9f45e90091748
yun <loverglory233@gmail.com>
2026-08-23 17:23:02 +0800

    feat: 升级install_etherpad_plugins.sh支持全套Word级学术富文本排版矩阵+升级v99

--- src/teacher.js
+++ src/teacher.js
@@ -9,8 +9,8 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260823_v98";
-import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v98";
+} from "./constants.js?v=20260823_v99";
+import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired } from "./utils.js?v=20260823_v99";
 
 /* ==========================================================================
    7. TEACHER PORTAL RENDERER (LIVE WORKSPACE MIRROR & ANNOUNCEMENT READ MATRIX)
