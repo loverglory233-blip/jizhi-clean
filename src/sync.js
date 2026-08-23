a9153faddd4add5250e0e4a4d4da3b4db485f496
yun <loverglory233@gmail.com>
2026-08-24 01:04:13 +0800

    fix: 提供restart_etherpad.sh解决502+优化进入任务先拉取权威状态彻底消除首屏空白与阶段跳变闪烁+升级v106

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v105';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v105';
+import { InitialState } from './constants.js?v=20260823_v106';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v106';
 
 export class CloudSyncEngine {
   constructor(app) {
