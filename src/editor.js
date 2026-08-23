e4d17c34101eb1f9dd7fad98e8a206fa78ea07b2
yun <loverglory233@gmail.com>
2026-08-24 01:45:52 +0800

    fix: 提供instant_fix_and_start.sh跳过GitHub SSH卡顿秒级拉起服务+升级v116

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260823_v115";
-import { callCozeAgentAPI } from "./agents.js?v=20260823_v115";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v115";
+import { AgentProfiles } from "./constants.js?v=20260823_v116";
+import { callCozeAgentAPI } from "./agents.js?v=20260823_v116";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired } from "./utils.js?v=20260823_v116";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
