06c59740e75e879eb5b14bac5109f7a4e9fd29e3
yun <loverglory233@gmail.com>
2026-08-24 07:34:10 +0800

    fix: 全面增强修改密码原密码多重识别支持明文/Bcrypt/MD5/初始123(v174)

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v173";
-import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v173";
+} from "./constants.js?v=20260823_v174";
+import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v174";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
--- src/teacher.js
+++ src/teacher.js
@@ -9,8 +9,8 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260823_v173";
-import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v173";
+} from "./constants.js?v=20260823_v174";
+import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260823_v174";
 
 /* ==========================================================================
    7. TEACHER PORTAL RENDERER (LIVE WORKSPACE MIRROR & ANNOUNCEMENT READ MATRIX)
