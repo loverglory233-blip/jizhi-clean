048aa9d3ba4cc7defbe8bc3d376eef99fd07b332
yun <loverglory233@gmail.com>
2026-08-24 08:40:06 +0800

    fix: 彻底打通多设备实时双向同步与无阻推送机制(v208)

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v207";
-import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v207";
+} from "./constants.js?v=20260823_v208";
+import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v208";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
