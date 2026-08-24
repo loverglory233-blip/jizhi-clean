651a31b51c7309310d219abf6c7899b8e46a0f28
yun <loverglory233@gmail.com>
2026-08-24 09:30:30 +0800

    fix: 优化自愈脚本PHP-FPM静默探测，全量支持多学生动态落库(v223)

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v222';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260823_v223';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey];
