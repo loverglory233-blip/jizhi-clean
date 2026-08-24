61a501226ea1970675a9d0e9909aa3eb5bb62779
yun <loverglory233@gmail.com>
2026-08-24 08:53:08 +0800

    fix: 彻底消除前端写死教师工号的残留代码，教师账号与密码全流程自由修改(v215)

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260823_v214";
-import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v214";
+} from "./constants.js?v=20260823_v215";
+import { escapeHtml, isTaskExpired, formatDurationHuman } from "./utils.js?v=20260823_v215";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
