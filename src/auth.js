dc0216895196ffb8a95d5dc89d4193b7bcb13535
yun <loverglory233@gmail.com>
2026-08-24 08:10:39 +0800

    fix: 彻底废除全任务通用并严格锁定通知与范文至具体写作任务(v198)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260823_v197';
-import { formatExportDateTime } from './utils.js?v=20260823_v197';
+} from './constants.js?v=20260823_v198';
+import { formatExportDateTime } from './utils.js?v=20260823_v198';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260823_v197';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v197';
+import { InitialState } from './constants.js?v=20260823_v198';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260823_v198';
 
 export class CloudSyncEngine {
   constructor(app) {
