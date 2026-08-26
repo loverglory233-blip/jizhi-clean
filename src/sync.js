d826225f505c1cb7c91cc57bddf938c0541f856e
yun <loverglory233@gmail.com>
2026-08-27 05:24:38 +0800

    fix(perf): 地毯式清理前端所有多余高频网络请求：移除1s get_pad_text轮询、移除冗余4s presence POST、全面接入后台挂机深度休眠节流 (20260827_v609)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260827_v608';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260827_v608';
+import { InitialState } from './constants.js?v=20260827_v609';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260827_v609';
 
 export class CloudSyncEngine {
   constructor(app) {
