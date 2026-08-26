e640267771bbbcab557b890d7943b476ca545335
yun <loverglory233@gmail.com>
2026-08-27 07:54:47 +0800

    chore(version): 全量版本戳与缓存穿透升级至 20260827_v613

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260827_v612";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired } from "./utils.js?v=20260827_v612";
-import { callCozeAgentAPI } from "./agents.js?v=20260827_v612";
-import { AuthManager } from "./auth.js?v=20260827_v612";
-import { CloudSyncEngine } from "./sync.js?v=20260827_v612";
-import { renderLoginView } from "./login.js?v=20260827_v612";
-import { renderTeacherPortal } from "./teacher.js?v=20260827_v612";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260827_v612";
+} from "./constants.js?v=20260827_v613";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired } from "./utils.js?v=20260827_v613";
+import { callCozeAgentAPI } from "./agents.js?v=20260827_v613";
+import { AuthManager } from "./auth.js?v=20260827_v613";
+import { CloudSyncEngine } from "./sync.js?v=20260827_v613";
+import { renderLoginView } from "./login.js?v=20260827_v613";
+import { renderTeacherPortal } from "./teacher.js?v=20260827_v613";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260827_v613";
 import {
   buildWordEditorHtml,
   attachWordEditorEvents,
@@ -26,7 +26,7 @@ import {
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260827_v612";
+} from "./editor.js?v=20260827_v613";
 
 // Make renderChat available on window for sync callbacks
 if (typeof window !== "undefined") {
