097670375a360e6df28e47b790e8fdfd55a3fc12
yun <loverglory233@gmail.com>
2026-08-27 08:29:48 +0800

    chore(version): 全量版本戳对齐升级至 20260827_v617

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260827_v616";
-import { callCozeAgentAPI } from "./agents.js?v=20260827_v616";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260827_v616";
+import { AgentProfiles } from "./constants.js?v=20260827_v617";
+import { callCozeAgentAPI } from "./agents.js?v=20260827_v617";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime } from "./utils.js?v=20260827_v617";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
