890f2c435e63f7ee234afa7952d531017b4204cc
yun <loverglory233@gmail.com>
2026-08-28 14:26:13 +0800

    fix: 教师监控阶段一左侧无滑动自然撑满且右侧对齐平齐、阶段二扩大至阶段一基准高度且文档自由滑动、阶段三答辩自适应与终稿统一 (20260828_v644)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260828_v643';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260828_v643';
+import { InitialState } from './constants.js?v=20260828_v644';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260828_v644';
 
 export class CloudSyncEngine {
   constructor(app) {
