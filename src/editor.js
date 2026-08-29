b9e00fc2b1e58eaad0905dbc93ae8c118949113a
yun <loverglory233@gmail.com>
2026-08-29 18:46:24 +0800

    fix: 完善插件根目录与src双向软链检索与激活 (20260829_v678)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260829_v677";
-import { callCozeAgentAPI } from "./agents.js?v=20260829_v677";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v677";
+import { AgentProfiles } from "./constants.js?v=20260829_v678";
+import { callCozeAgentAPI } from "./agents.js?v=20260829_v678";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v678";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
