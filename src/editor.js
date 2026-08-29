325b692f3dd032ace03752d97b40e06eb9fb150c
yun <loverglory233@gmail.com>
2026-08-29 18:22:37 +0800

    feat: 采用防御式全量挂载装载12大核心学术插件与工具栏 (20260829_v670)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260829_v669";
-import { callCozeAgentAPI } from "./agents.js?v=20260829_v669";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v669";
+import { AgentProfiles } from "./constants.js?v=20260829_v670";
+import { callCozeAgentAPI } from "./agents.js?v=20260829_v670";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v670";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
