13fa79f8d74ed31d9fd58ae886db4efc70f5697b
yun <loverglory233@gmail.com>
2026-08-30 01:52:14 +0800

    fix(milestone): 增加组内单端代表触发仲裁与历史消息排重，彻底杜绝初审双触发重复冒泡 (20260830_v708)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v707";
+import { escapeHtml } from "./utils.js?v=20260830_v708";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v707';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v707';
+import { InitialState } from './constants.js?v=20260830_v708';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v708';
 
 export class CloudSyncEngine {
   constructor(app) {
