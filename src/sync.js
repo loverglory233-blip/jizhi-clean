a42e1343377371fd95f1e88fb102380681628ade
yun <loverglory233@gmail.com>
2026-08-29 22:44:36 +0800

    feat: 以4000字学术基准精准标定宏观三层质检触发逻辑 (20260829_v691)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260829_v690';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v690';
+import { InitialState } from './constants.js?v=20260829_v691';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v691';
 
 export class CloudSyncEngine {
   constructor(app) {
