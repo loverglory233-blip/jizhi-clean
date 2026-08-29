5d5d9ea319ba89424deb6cdeca528dea3b781d0c
yun <loverglory233@gmail.com>
2026-08-29 08:57:49 +0800

    release: 全量版本戳升级至 20260829_v652，强制击穿浏览器所有静态与模块缓存

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260829_v651";
-import { callCozeAgentAPI } from "./agents.js?v=20260829_v651";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v651";
+import { AgentProfiles } from "./constants.js?v=20260829_v652";
+import { callCozeAgentAPI } from "./agents.js?v=20260829_v652";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v652";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
