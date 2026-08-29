4c37949e47913ab72c12551345243840441077a7
yun <loverglory233@gmail.com>
2026-08-29 20:51:56 +0800

    fix: v688 修复Nginx 443代理+Etherpad 2.x启动

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v687';
-import { formatExportDateTime } from './utils.js?v=20260829_v687';
+} from './constants.js?v=20260829_v688';
+import { formatExportDateTime } from './utils.js?v=20260829_v688';
 
 export class AuthManager {
   constructor() {
