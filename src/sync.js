341ec94a3128d0ed5dd679689b4785485db31fe9
yun <loverglory233@gmail.com>
2026-08-29 19:03:18 +0800

    fix: 彻底清空数据库旧模版字数并增强Etherpad动态模块探测 (20260829_v684)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260829_v683';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v683';
+import { InitialState } from './constants.js?v=20260829_v684';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v684';
 
 export class CloudSyncEngine {
   constructor(app) {
