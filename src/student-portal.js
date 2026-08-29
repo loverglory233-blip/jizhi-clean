c0a98406b329d8ba71520e5427da33b30a3f0790
yun <loverglory233@gmail.com>
2026-08-29 17:43:53 +0800

    feat: 纯化公约生成题目链路(100%由大模型通读全组研讨实时返回) (20260829_v659)

--- src/student-portal.js
+++ src/student-portal.js
@@ -7,8 +7,8 @@ import {
   STORAGE_KEY_TASKS,
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES
-} from "./constants.js?v=20260829_v658";
-import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice } from "./utils.js?v=20260829_v658";
+} from "./constants.js?v=20260829_v659";
+import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice } from "./utils.js?v=20260829_v659";
 
 /* ==========================================================================
    7.5 STUDENT TASK PORTAL / DASHBOARD (我的写作任务大厅)
