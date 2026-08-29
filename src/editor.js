771d5c27ecba763ebcf7b46407c33b02adebab2b
yun <loverglory233@gmail.com>
2026-08-29 20:51:56 +0800

    fix: v688 修复Nginx 443代理+Etherpad 2.x启动

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260829_v687";
-import { callCozeAgentAPI } from "./agents.js?v=20260829_v687";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v687";
+import { AgentProfiles } from "./constants.js?v=20260829_v688";
+import { callCozeAgentAPI } from "./agents.js?v=20260829_v688";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v688";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260829_v687';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v687';
+import { InitialState } from './constants.js?v=20260829_v688';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260829_v688';
 
 export class CloudSyncEngine {
   constructor(app) {
