78f72d2144542e5e1873f7f9008699ab39864a18
yun <loverglory233@gmail.com>
2026-08-29 18:29:00 +0800

    feat: 完善所有Node环境变量多路径检索与独立新窗口直通调试通道 (20260829_v671)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v670';
-import { formatExportDateTime } from './utils.js?v=20260829_v670';
+} from './constants.js?v=20260829_v671';
+import { formatExportDateTime } from './utils.js?v=20260829_v671';
 
 export class AuthManager {
   constructor() {
