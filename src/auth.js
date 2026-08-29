392353c954b8d8512791f6d309ed6485bc26e9f8
yun <loverglory233@gmail.com>
2026-08-29 18:11:47 +0800

    fix: 全链路打通Etherpad极速自愈、环境变量挂载与Nginx反代连通性 (20260829_v667)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v666';
-import { formatExportDateTime } from './utils.js?v=20260829_v666';
+} from './constants.js?v=20260829_v667';
+import { formatExportDateTime } from './utils.js?v=20260829_v667';
 
 export class AuthManager {
   constructor() {
