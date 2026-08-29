1d62363f9292d69535f9664e048d99c225a60fc1
yun <loverglory233@gmail.com>
2026-08-29 17:33:44 +0800

    feat: 拍卖师大模型融合学术标题深度提炼、修复监控小组下拉切换与在线状态防抖 (20260829_v656)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v655';
-import { formatExportDateTime } from './utils.js?v=20260829_v655';
+} from './constants.js?v=20260829_v656';
+import { formatExportDateTime } from './utils.js?v=20260829_v656';
 
 export class AuthManager {
   constructor() {
