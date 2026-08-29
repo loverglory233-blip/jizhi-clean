56f7fc29c716b79efdb8123d1d9cfd7a74e38a41
yun <loverglory233@gmail.com>
2026-08-30 02:13:16 +0800

    feat(chat): 在初稿确认时即时向右侧聊天区全组广播确认进度与责任编辑督促 (20260830_v721)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v720';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v720';
+import { InitialState } from './constants.js?v=20260830_v721';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v721';
 
 export class CloudSyncEngine {
   constructor(app) {
