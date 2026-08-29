7386a186432bcffdfdcfe4b4bb4e14e51d43a041
yun <loverglory233@gmail.com>
2026-08-29 18:52:37 +0800

    fix: 完善pad静态资源检测与Nginx iframe跨域支持 (20260829_v680)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v679';
-import { formatExportDateTime } from './utils.js?v=20260829_v679';
+} from './constants.js?v=20260829_v680';
+import { formatExportDateTime } from './utils.js?v=20260829_v680';
 
 export class AuthManager {
   constructor() {
