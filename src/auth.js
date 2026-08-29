5569b3911f05bf4a89831f22f2a218a578dbbd98
yun <loverglory233@gmail.com>
2026-08-29 18:45:19 +0800

    fix: 采用Node安全重构Nginx所有站点配置文件杜绝重复块与500报错 (20260829_v677)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260829_v676';
-import { formatExportDateTime } from './utils.js?v=20260829_v676';
+} from './constants.js?v=20260829_v677';
+import { formatExportDateTime } from './utils.js?v=20260829_v677';
 
 export class AuthManager {
   constructor() {
--- src/teacher.js
+++ src/teacher.js
@@ -9,8 +9,8 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260829_v676";
-import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime, formatStandardDateDash, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v676";
+} from "./constants.js?v=20260829_v677";
+import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime, formatStandardDateDash, filterAndDeduplicateChatLogs } from "./utils.js?v=20260829_v677";
 
 /* ==========================================================================
    7. TEACHER PORTAL RENDERER (LIVE WORKSPACE MIRROR & ANNOUNCEMENT READ MATRIX)
