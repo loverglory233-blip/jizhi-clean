695bd7aeaae8e749cb567c4d3df06b2c238223ea
yun <loverglory233@gmail.com>
2026-08-30 02:10:08 +0800

    fix: 修复 handlers 对象字面量末尾分号语法错误 (20260830_v719)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v718';
-import { formatExportDateTime } from './utils.js?v=20260830_v718';
+} from './constants.js?v=20260830_v719';
+import { formatExportDateTime } from './utils.js?v=20260830_v719';
 
 export class AuthManager {
   constructor() {
