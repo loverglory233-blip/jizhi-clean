316624bad9038f3a064b77e94123183c154ca741
yun <loverglory233@gmail.com>
2026-08-30 16:33:39 +0800

    feat: 升级方案细化Prompt为自适应二元研判——宽进肯定亮点顺承引导分工，红线温和启发 (20260830_v841)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v840';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v840';
+} from './constants.js?v=20260830_v841';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v841';
 
 export class AuthManager {
   constructor() {
