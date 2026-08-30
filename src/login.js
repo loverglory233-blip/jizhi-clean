bf60d65b0fa517543bc9e622330d085e77030e14
yun <loverglory233@gmail.com>
2026-08-31 00:16:21 +0800

    fix: 将阶段二【让责任编辑总结】和【让审稿编辑总结】按钮精准设置在聊天输入框表情栏正上方浮动条中 (20260831_v923)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260831_v922";
+import { escapeHtml } from "./utils.js?v=20260831_v923";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260831_v922';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260831_v922';
+import { InitialState } from './constants.js?v=20260831_v923';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260831_v923';
 
 export class CloudSyncEngine {
   constructor(app) {
