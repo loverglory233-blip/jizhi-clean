cd40e220bf88354b2cfc0619e697e594127194fa
yun <loverglory233@gmail.com>
2026-08-31 01:53:45 +0800

    fix: 正确声明 newPresenceHtml 变量，彻底消除长轮询控制台 ReferenceError 报错 (20260831_v967)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v966';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v966';
+} from './constants.js?v=20260831_v967';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v967';
 
 export class AuthManager {
   constructor() {
