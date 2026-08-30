4c7cc516951d8abd8843fe03b7dedcf56ed4fcc6
yun <loverglory233@gmail.com>
2026-08-31 00:43:39 +0800

    fix: 完善公约全员签署矩阵实时动态响应与 2/2 达成瞬间直接平滑进入阶段二 Etherpad 实时协同撰写画布 (20260831_v933)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v932';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v932';
+} from './constants.js?v=20260831_v933';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v933';
 
 export class AuthManager {
   constructor() {
