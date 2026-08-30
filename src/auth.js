3655820f18fc08f10e88768d1cc95ed99ce82061
yun <loverglory233@gmail.com>
2026-08-30 16:32:05 +0800

    fix: 显式持久化阶段一flowStep状态机+全员多端强同步与心跳无缝托底 (20260830_v840)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v839';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v839';
+} from './constants.js?v=20260830_v840';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v840';
 
 export class AuthManager {
   constructor() {
