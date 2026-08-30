9aec5ffd80902f371daa0fb39e6e54c0f2f54923
yun <loverglory233@gmail.com>
2026-08-30 10:21:35 +0800

    fix(student-portal): 彻底打通任务大厅延期事件监听、自动刷新任务卡片状态与下发顶部通知 (20260830_v764)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v763';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v763';
+} from './constants.js?v=20260830_v764';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v764';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v763';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v763';
+import { InitialState } from './constants.js?v=20260830_v764';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v764';
 
 export class CloudSyncEngine {
   constructor(app) {
