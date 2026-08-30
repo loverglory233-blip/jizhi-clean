73c40effb7d1022da41858a3a387f2cc58d2fcfa
yun <loverglory233@gmail.com>
2026-08-31 01:28:26 +0800

    fix: 实现贡献度全键名兼容提取与前端乐观 0 延迟即时重绘机制 (20260831_v955)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260831_v954';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260831_v954';
+import { InitialState } from './constants.js?v=20260831_v955';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260831_v955';
 
 export class CloudSyncEngine {
   constructor(app) {
