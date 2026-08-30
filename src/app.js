713b7304979801853d895df08474d960b98aa32e
yun <loverglory233@gmail.com>
2026-08-30 23:09:23 +0800

    fix: 彻底铲除所有假兜底与假回复+100%纯净直连Coze大模型真实生成+真实超时提示 (20260830_v907)

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260830_v906";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isScopeMatch } from "./utils.js?v=20260830_v906";
-import { callCozeAgentAPI } from "./agents.js?v=20260830_v906";
-import { AuthManager } from "./auth.js?v=20260830_v906";
-import { CloudSyncEngine } from "./sync.js?v=20260830_v906";
-import { renderLoginView } from "./login.js?v=20260830_v906";
-import { renderTeacherPortal } from "./teacher.js?v=20260830_v906";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260830_v906";
+} from "./constants.js?v=20260830_v907";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isScopeMatch } from "./utils.js?v=20260830_v907";
+import { callCozeAgentAPI } from "./agents.js?v=20260830_v907";
+import { AuthManager } from "./auth.js?v=20260830_v907";
+import { CloudSyncEngine } from "./sync.js?v=20260830_v907";
+import { renderLoginView } from "./login.js?v=20260830_v907";
+import { renderTeacherPortal } from "./teacher.js?v=20260830_v907";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260830_v907";
 import {
   buildWordEditorHtml,
   attachWordEditorEvents,
@@ -26,7 +26,7 @@ import {
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260830_v906";
+} from "./editor.js?v=20260830_v907";
 
 // Make renderChat available on window for sync callbacks
 if (typeof window !== "undefined") {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v906';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v906';
+import { InitialState } from './constants.js?v=20260830_v907';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v907';
 
 export class CloudSyncEngine {
   constructor(app) {
