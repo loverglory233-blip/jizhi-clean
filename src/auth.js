cf4e1520de8ba543e17ea5f8de58d49048af36f8
yun <loverglory233@gmail.com>
2026-08-31 00:55:40 +0800

    fix: 重构阶段二排版为紧凑一体化设计并引入同源 iframe 毫秒级 DOM 直读同步字数与贡献度 (20260831_v939)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v938';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v938';
+} from './constants.js?v=20260831_v939';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v939';
 
 export class AuthManager {
   constructor() {
