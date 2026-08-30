0eaf67bfd62d6d71f4297258eaacc8b357a7ec2a
yun <loverglory233@gmail.com>
2026-08-30 16:55:59 +0800

    fix: 前端纯时钟静默防抖+大模型通读宽进研判架构全面锁死落地 (20260830_v847)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v846';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v846';
+} from './constants.js?v=20260830_v847';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v847';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v846";
+import { escapeHtml } from "./utils.js?v=20260830_v847";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
