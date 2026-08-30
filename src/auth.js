d752631ef45acdeaf0295d126252668ff14af032
yun <loverglory233@gmail.com>
2026-08-30 09:53:31 +0800

    fix: 全面实施物理级只读透明护盾与禁止图标(cursor:not-allowed)+滚轮精准透传 (20260830_v758)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v757';
-import { formatExportDateTime } from './utils.js?v=20260830_v757';
+} from './constants.js?v=20260830_v758';
+import { formatExportDateTime } from './utils.js?v=20260830_v758';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v757';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v757';
+import { InitialState } from './constants.js?v=20260830_v758';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin } from './utils.js?v=20260830_v758';
 
 export class CloudSyncEngine {
   constructor(app) {
