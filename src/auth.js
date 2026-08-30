53424a7a317b60e207b859325beedceac29c4c40
yun <loverglory233@gmail.com>
2026-08-31 00:31:57 +0800

    feat: 完善2/2达成时的提炼中动画状态与聊天区生成成功播报通知 (20260831_v928)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v927';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v927';
+} from './constants.js?v=20260831_v928';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v928';
 
 export class AuthManager {
   constructor() {
