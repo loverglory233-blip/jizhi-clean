2fc46c60534bad9fd46cc81efee82968b83b746f
yun <loverglory233@gmail.com>
2026-08-30 12:22:46 +0800

    fix(revert): 100%还原只读滑动锁定实现，精准保留视图记忆 (20260830_v787)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v786';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v786';
+import { InitialState } from './constants.js?v=20260830_v787';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v787';
 
 export class CloudSyncEngine {
   constructor(app) {
