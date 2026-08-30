970ae486ae3ab530b5f94a5cd0f297394d01e4e5
yun <loverglory233@gmail.com>
2026-08-30 17:51:05 +0800

    release: 工业级纯净数据驱动终版发布，全面通过60人30组容量压测审计 (20260830_v858)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v857";
+import { escapeHtml } from "./utils.js?v=20260830_v858";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/teacher.js
+++ src/teacher.js
@@ -9,8 +9,8 @@ import {
   STORAGE_KEY_CLASSES,
   STORAGE_KEY_USERS_DB,
   AgentProfiles
-} from "./constants.js?v=20260830_v857";
-import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime, formatStandardDateDash, filterAndDeduplicateChatLogs, enforceEtherpadReadonly, showGlobalBannerNotice } from "./utils.js?v=20260830_v857";
+} from "./constants.js?v=20260830_v858";
+import { parseXLSXOrCSVFile, parseCSVText, downloadFileBlob, escapeHtml, isTaskExpired, formatDurationHuman, formatChatDisplayTime, formatStandardDateDash, filterAndDeduplicateChatLogs, enforceEtherpadReadonly, showGlobalBannerNotice } from "./utils.js?v=20260830_v858";
 
 /* ==========================================================================
    7. TEACHER PORTAL RENDERER (LIVE WORKSPACE MIRROR & ANNOUNCEMENT READ MATRIX)
