60f8125b37a91ae193221a5c7c994c7a81115fb4
yun <loverglory233@gmail.com>
2026-08-30 17:57:29 +0800

    fix: 严格恢复静默守护破冰机制为不干活时最多连续提醒2次+活动自动重置 (20260830_v863)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v862';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v862';
+} from './constants.js?v=20260830_v863';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v863';
 
 export class AuthManager {
   constructor() {
