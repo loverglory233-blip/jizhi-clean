186f4d0b98d33c955b7aaef4e195903e5968a3a0
yun <loverglory233@gmail.com>
2026-08-31 01:15:20 +0800

    feat: 严格支持自适应大小任务与教师自定义字数动态计算 30%/65%/90% 学术质检水位线 (20260831_v948)

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260831_v947';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260831_v948';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey] || { name: '智能体专家', avatar: '🤖' };
