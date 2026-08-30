c6642f9239a8ecc6df9b5cc87b20bd38ae7a5066
yun <loverglory233@gmail.com>
2026-08-31 00:14:25 +0800

    feat: 补齐阶段二【🤝 讨论差不多了？让责任编辑总结】和【📝 让审稿编辑总结】动态流转按钮，优化阶段一落槌调用状态反馈 (20260831_v922)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v921';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v921';
+} from './constants.js?v=20260831_v922';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v922';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260831_v921";
+import { escapeHtml } from "./utils.js?v=20260831_v922";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
