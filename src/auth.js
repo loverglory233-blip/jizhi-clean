84a0b2535cdac658b0f4b1aa118a71a748393fec
yun <loverglory233@gmail.com>
2026-08-30 19:34:21 +0800

    feat: 阶段二第三次质检支持85%时间进度由责任编辑下发倒计时收尾提醒并联动审稿编辑终审定稿扫描 (20260830_v874)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v873';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v873';
+} from './constants.js?v=20260830_v874';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v874';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v873';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v873';
+import { InitialState } from './constants.js?v=20260830_v874';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v874';
 
 export class CloudSyncEngine {
   constructor(app) {
