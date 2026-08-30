07768f7662a67f7a744ed9eeabdc3dd7eed943a6
yun <loverglory233@gmail.com>
2026-08-30 15:12:07 +0800

    fix: 彻底消除system重复消息+投票与共识改由拍卖师两句式清晰说明题目与引导+消除串行大模型调用全面提速 (20260830_v817)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v816';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v816';
+} from './constants.js?v=20260830_v817';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v817';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v816";
+import { escapeHtml } from "./utils.js?v=20260830_v817";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
