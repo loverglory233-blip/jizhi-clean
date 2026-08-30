253d3ac93ff9f9e6decd09dc0753189b67011ab0
yun <loverglory233@gmail.com>
2026-08-31 02:20:42 +0800

    refactor: 彻底删除多余的阶段二日常研讨破冰，严格聚焦一审建议静默与半程打卡静默 (20260831_v984)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v983';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v983';
+} from './constants.js?v=20260831_v984';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v984';
 
 export class AuthManager {
   constructor() {
