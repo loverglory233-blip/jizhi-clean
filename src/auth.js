671dbeb069f434209c68f48600efee650c43739f
yun <loverglory233@gmail.com>
2026-08-30 16:37:14 +0800

    feat: 拍卖师/责任编辑/审稿编辑三大智能体全量设定与研判流转100%全流程对齐装配 (20260830_v843)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v842';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v842';
+} from './constants.js?v=20260830_v843';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v843';
 
 export class AuthManager {
   constructor() {
