30d15ce09c5ac72012618e286eb5cc270da27e24
yun <loverglory233@gmail.com>
2026-08-30 17:16:42 +0800

    fix: 严密落实班级+小组+任务三位一体精准靶向过滤与0延迟已读消除 (20260830_v853)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v852';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v852';
+} from './constants.js?v=20260830_v853';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v853';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v852';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v852';
+import { InitialState } from './constants.js?v=20260830_v853';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v853';
 
 export class CloudSyncEngine {
   constructor(app) {
