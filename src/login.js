ea8eb8e3151ea84b79bc97d49c365f19694592b0
yun <loverglory233@gmail.com>
2026-08-31 02:01:49 +0800

    fix: 解除静默巡检的第一顺位单点限制，确保任意组员账号在冷场满阈值时均可触发静默提醒 (20260831_v971)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260831_v970";
+import { escapeHtml } from "./utils.js?v=20260831_v971";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
