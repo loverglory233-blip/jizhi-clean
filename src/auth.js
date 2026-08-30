355a8605dd8abaccdb025962226d3627618c57cd
yun <loverglory233@gmail.com>
2026-08-30 19:49:52 +0800

    feat: 彻底清理全部非必要打卡广播+严格确立大模型调用与写死开场/倒计时/静默边界 (20260830_v877)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v876';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v876';
+} from './constants.js?v=20260830_v877';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v877';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v876";
+import { escapeHtml } from "./utils.js?v=20260830_v877";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
