73f90ac7f69007de3b63e63d6d9044053d9e3e38
yun <loverglory233@gmail.com>
2026-08-30 13:45:39 +0800

    fix: 统一教师端与学生端只读遮罩 pointer-events:none + 全局版本升级 (20260830_v798)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v797";
+import { escapeHtml } from "./utils.js?v=20260830_v798";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
