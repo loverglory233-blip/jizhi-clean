badedffa0302ad1c2c1ff8c232e85af52bfa5bba
yun <loverglory233@gmail.com>
2026-08-30 10:49:21 +0800

    fix(teacher): 彻底移除前端异步阻塞锁，点击延期0ms关闭弹窗并更新界面 (20260830_v771)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260830_v770";
+import { escapeHtml } from "./utils.js?v=20260830_v771";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v770';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v770';
+import { InitialState } from './constants.js?v=20260830_v771';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v771';
 
 export class CloudSyncEngine {
   constructor(app) {
