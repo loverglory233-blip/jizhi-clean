c459676e06c04cfb9add2e42fdf607d5b3200d47
yun <loverglory233@gmail.com>
2026-08-30 22:10:33 +0800

    feat: 加固离线静默原则(离线期间发生的创建/延期/删除事件在学生上线时绝不补发弹窗或历史横幅) (20260830_v890)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260830_v889';
-import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v889';
+} from './constants.js?v=20260830_v890';
+import { formatExportDateTime, formatDurationHuman } from './utils.js?v=20260830_v890';
 
 export class AuthManager {
   constructor() {
