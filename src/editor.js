c3973e2c2e09c70a9b24094a2407839be5a12b4c
yun <loverglory233@gmail.com>
2026-08-30 15:45:20 +0800

    release: 阶段一细化方案全场景语义识别+教师端轻量秒开+聊天输入体验优化终版 (20260830_v830)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260830_v829";
-import { callCozeAgentAPI } from "./agents.js?v=20260830_v829";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs, enforceEtherpadReadonly } from "./utils.js?v=20260830_v829";
+import { AgentProfiles } from "./constants.js?v=20260830_v830";
+import { callCozeAgentAPI } from "./agents.js?v=20260830_v830";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs, enforceEtherpadReadonly } from "./utils.js?v=20260830_v830";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v829';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v829';
+import { InitialState } from './constants.js?v=20260830_v830';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v830';
 
 export class CloudSyncEngine {
   constructor(app) {
