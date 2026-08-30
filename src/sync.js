2cb4175196c76730b1df3fdb9a5efcd8d5d67fbe
yun <loverglory233@gmail.com>
2026-08-30 14:52:49 +0800

    feat: 恢复情绪温暖写死兜底+AI异常时贴心引导@智能体重新召唤 (20260830_v811)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260830_v810';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v810';
+import { InitialState } from './constants.js?v=20260830_v811';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal } from './utils.js?v=20260830_v811';
 
 export class CloudSyncEngine {
   constructor(app) {
