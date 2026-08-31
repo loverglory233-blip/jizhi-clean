f0b4f8fd7cb3780c1afc736582e5c32f14e6de43
yun <loverglory233@gmail.com>
2026-08-31 09:08:41 +0800

    feat: 注入全组自查瓶颈数据并全面贯彻【诊断问题+改进建议】二审双结构规范 (20260831_v1003)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v1002';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v1002';
+} from './constants.js?v=20260831_v1003';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v1003';
 
 export class AuthManager {
   constructor() {
