19e845845b53212ebef5c06be83db642d6ab750c
yun <loverglory233@gmail.com>
2026-08-31 09:30:39 +0800

    fix: 彻底剔除重复冗余的第二套兜底决议逻辑，确保半程研讨仅有唯一一次修改小结 (20260831_v1018)

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260831_v1017";
+import { escapeHtml } from "./utils.js?v=20260831_v1018";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
