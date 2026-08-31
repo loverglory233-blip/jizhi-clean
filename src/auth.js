f511f40281bc93c1a956397c9e95d5ba5833f0b2
yun <loverglory233@gmail.com>
2026-08-31 09:39:59 +0800

    feat: 补全全员打卡后责任编辑一致性分析的聊天区思考气泡，全流程大模型研判全覆盖 (20260831_v1022)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260831_v1021';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v1021';
+} from './constants.js?v=20260831_v1022';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260831_v1022';
 
 export class AuthManager {
   constructor() {
