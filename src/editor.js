bcbfc3639a0a9089a9b7273590e92d7dad756c7c
yun <loverglory233@gmail.com>
2026-09-01 00:39:37 +0800

    feat: 落地庆祝弹窗方案C(5秒倒计时+即点即进)与终稿提交全自动唤起问卷弹窗 (升级v1113)

--- src/editor.js
+++ src/editor.js
@@ -3,9 +3,9 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles } from "./constants.js?v=20260901_v1112";
-import { callCozeAgentAPI } from "./agents.js?v=20260901_v1112";
-import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs, enforceEtherpadReadonly, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, showResolutionBlock } from "./utils.js?v=20260901_v1112";
+import { AgentProfiles } from "./constants.js?v=20260901_v1113";
+import { callCozeAgentAPI } from "./agents.js?v=20260901_v1113";
+import { downloadFileBlob, getCaretCharacterOffsetWithin, setCaretPositionWithin, escapeHtml, sanitizeUrl, isTaskExpired, formatDurationHuman, formatChatDisplayTime, filterAndDeduplicateChatLogs, enforceEtherpadReadonly, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, showResolutionBlock } from "./utils.js?v=20260901_v1113";
 
 /* ==========================================================================
    8. UI RENDERER (STUDENT CANVAS & HEADER)
