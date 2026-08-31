61d51c2debcb4e92450a3c66f081c0513f896841
yun <loverglory233@gmail.com>
2026-09-01 02:43:34 +0800

    feat: 教师端发布/修改任务新增目标字数配置(默认3000字)，彻底解耦大小任务标签 (升级v1132)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260901_v1131';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260901_v1131';
+import { InitialState } from './constants.js?v=20260901_v1132';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260901_v1132';
 
 export class CloudSyncEngine {
   constructor(app) {
