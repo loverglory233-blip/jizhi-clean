2db732291afdd5f305940669a0c4318a3f601f41
yun <loverglory233@gmail.com>
2026-09-01 01:19:15 +0800

    feat: 半程会议弹窗自查题目、章节复选框、痛点瓶颈与占位符根据写作文体全自适应动态切换 (升级v1120)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260901_v1119';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260901_v1119';
+import { InitialState } from './constants.js?v=20260901_v1120';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260901_v1120';
 
 export class CloudSyncEngine {
   constructor(app) {
