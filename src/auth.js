3d277f8eaee810b102ea39d8514ef35bbb49a3f4
yun <loverglory233@gmail.com>
2026-09-03 23:07:58 +0800

    feat(genre): 深度完善【教学设计方案】专属Prompt体系与考查维度 (v2080)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260903_v2075';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v2075';
+} from './constants.js?v=20260903_v2080';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v2080';
 
 export class AuthManager {
   constructor() {
