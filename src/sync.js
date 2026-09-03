61d1b08b5fa15293081dad98ae29176da189c448
yun <loverglory233@gmail.com>
2026-09-03 23:15:24 +0800

    feat(prompt): 审稿意见全面重构为直击要害【哪里有什么问题->怎么改】，一审增加后续写没写条件分支，教学设计专属Prompt完整对齐 (v2090)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260903_v2085';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2085';
+import { InitialState } from './constants.js?v=20260903_v2090';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2090';
 
 export class CloudSyncEngine {
   constructor(app) {
