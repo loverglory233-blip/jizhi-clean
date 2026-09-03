8098014c92ef17f1fe664f53f923b3efe669ad5d
yun <loverglory233@gmail.com>
2026-09-03 23:54:24 +0800

    fix(syntax): 清理第 1517 行多余花括号，彻底解决 stage3 结构闭合问题 (v2125)

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260903_v2120';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260903_v2125';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey] || { name: '智能体专家', avatar: '🤖' };
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260903_v2120';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2120';
+import { InitialState } from './constants.js?v=20260903_v2125';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2125';
 
 export class CloudSyncEngine {
   constructor(app) {
