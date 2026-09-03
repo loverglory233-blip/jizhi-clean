a55646b8bec66fad20a94230ad2c830f77100d86
yun <loverglory233@gmail.com>
2026-09-03 22:27:30 +0800

    feat(checkin): 强化静默判定基于讨论区发言，3分钟未打卡无论在干嘛直接弹出自查打卡窗口 (v2020)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260903_v2015';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v2015';
+} from './constants.js?v=20260903_v2020';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v2020';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260903_v2015';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2015';
+import { InitialState } from './constants.js?v=20260903_v2020';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v2020';
 
 export class CloudSyncEngine {
   constructor(app) {
