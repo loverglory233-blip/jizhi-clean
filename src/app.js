1a33e8513f1e71afbc3ea8c4c1695d354f8a6dad
yun <loverglory233@gmail.com>
2026-09-03 14:43:45 +0800

    release: 发布 20260903_v1444 版本，全平台学生总库与批量删除功能上线

--- src/app.js
+++ src/app.js
@@ -13,14 +13,14 @@ import {
   getAgentDisplayName,
   getGenrePromptDescriptor,
   AgentProfiles
-} from "./constants.js?v=20260903_v1326";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isScopeMatch, showResolutionBlock } from "./utils.js?v=20260903_v1326";
-import { callCozeAgentAPI } from "./agents.js?v=20260903_v1326";
-import { AuthManager } from "./auth.js?v=20260903_v1326";
-import { CloudSyncEngine } from "./sync.js?v=20260903_v1326";
-import { renderLoginView } from "./login.js?v=20260903_v1326";
-import { renderTeacherPortal } from "./teacher.js?v=20260903_v1326";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260903_v1326";
+} from "./constants.js?v=20260903_v1444";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isScopeMatch, showResolutionBlock } from "./utils.js?v=20260903_v1444";
+import { callCozeAgentAPI } from "./agents.js?v=20260903_v1444";
+import { AuthManager } from "./auth.js?v=20260903_v1444";
+import { CloudSyncEngine } from "./sync.js?v=20260903_v1444";
+import { renderLoginView } from "./login.js?v=20260903_v1444";
+import { renderTeacherPortal } from "./teacher.js?v=20260903_v1444";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260903_v1444";
 import {
   buildWordEditorHtml,
   attachWordEditorEvents,
@@ -29,7 +29,7 @@ import {
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260903_v1326";
+} from "./editor.js?v=20260903_v1444";
 
 // Make renderChat available on window for sync callbacks and listen to global IME composition
 if (typeof window !== "undefined") {
--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260903_v1326';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v1326';
+} from './constants.js?v=20260903_v1444';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v1444';
 
 export class AuthManager {
   constructor() {
