4c9f56cfcc35509b289b2e050fcf0a4d47112366
yun <loverglory233@gmail.com>
2026-09-03 19:07:49 +0800

    feat(agents): 严密落实智能体阶段强隔离与审稿编辑基于文献前置条件的发言时序 (v1910)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260903_v1905';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v1905';
+} from './constants.js?v=20260903_v1910';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260903_v1910';
 
 export class AuthManager {
   constructor() {
