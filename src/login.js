f5f4b892874a9755edecb5e08ac8b9affeb4b8ae
yun <loverglory233@gmail.com>
2026-09-03 17:05:09 +0800

    refactor: 全面采用主流 UI 库标准模态弹窗范式，消除遮罩双重滚动条与硬编码拦截

--- src/login.js
+++ src/login.js
@@ -1,4 +1,4 @@
-import { escapeHtml } from "./utils.js?v=20260903_v1703";
+import { escapeHtml } from "./utils.js?v=20260903_v1705";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState } from './constants.js?v=20260903_v1703';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v1703';
+import { InitialState } from './constants.js?v=20260903_v1705';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap } from './utils.js?v=20260903_v1705';
 
 export class CloudSyncEngine {
   constructor(app) {
