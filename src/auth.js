8a86973d4dfc394e599348a398d118b2e055daa1
yun <loverglory233@gmail.com>
2026-09-05 04:44:34 +0800

    fix(chat-sync): fix groupId scope reference in sendSingleChatMessage and prevent duplicate stacked inactivity messages on resume from idle (v2671)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2670';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2670';
+} from './constants.js?v=20260905_v2671';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2671';
 
 export class AuthManager {
   constructor() {
