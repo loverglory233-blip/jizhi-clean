d451d4e98f409bb5029547bd767bb9f2ce85080c
yun <loverglory233@gmail.com>
2026-09-04 17:53:05 +0800

    fix: auto-heal dirty milestone flags to guarantee 100% reliable first review trigger (v20260904_v2385)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2380';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2380';
+} from './constants.js?v=20260904_v2385';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2385';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2380";
-import { APP_VERSION } from "./constants.js?v=20260904_v2380";
+import { escapeHtml } from "./utils.js?v=20260904_v2385";
+import { APP_VERSION } from "./constants.js?v=20260904_v2385";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
