9e37d95877f1e2fe3852a6fe49963160a96f49b3
yun <loverglory233@gmail.com>
2026-09-04 17:41:09 +0800

    feat: recap contract division of labor and time planning in managing editor opening speech (v20260904_v2365)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2360';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2360';
+} from './constants.js?v=20260904_v2365';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2365';
 
 export class AuthManager {
   constructor() {
