8d25670839cddb4b2a41da50df63db2fddba963a
yun <loverglory233@gmail.com>
2026-09-05 01:02:00 +0800

    fix: 延期解锁时禁止重载iframe防止状态重置，确保编辑内容平滑无感延续 (v20260904_v2527)

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,6 @@
 /**
  * JIZHI (集智) Multi-Agent Collaborative Writing Platform
- * Version: 20260904_v2526
+ * Version: 20260904_v2527
  * Modern ES Module Distribution Bundle
  * (Compiled from src/*.js via build.py)
  */
@@ -16,7 +16,7 @@
    * Version: 2.1.0 (2026-08-23)
    */
 
-  const APP_VERSION = '20260904_v2526';
+  const APP_VERSION = '20260904_v2527';
   const APP_BUILD_DATE = '2026-09-04';
 
   const STORAGE_KEY_USER = 'jizhi_pure_v10_user';
@@ -11549,10 +11549,7 @@
             enforceEtherpadReadonly(existingFrame);
           } else {
             liftEtherpadReadonly(existingFrame);
-            if (existingFrame._wasPreviouslyReadonly) {
-              existingFrame._wasPreviouslyReadonly = false;
-              existingFrame.src = padUrl;
-            }
+            existingFrame._wasPreviouslyReadonly = false;
           }
         }
 
--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2526';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2526';
+} from './constants.js?v=20260904_v2527';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2527';
 
 export class AuthManager {
   constructor() {
