d32a5dd42a0e43f5e65785afee48f6dcbda9eb2a
yun <loverglory233@gmail.com>
2026-09-05 02:15:44 +0800

    fix(sync): fix group_states last_timestamp column in SQL queries and add proactive mirror alignment for teacher monitor (v20260905_v2560)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260905_v2555';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260905_v2555';
+import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260905_v2560';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260905_v2560';
 
 export class CloudSyncEngine {
   constructor(app) {
