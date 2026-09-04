308d9a1c6bcfe7a02769e71563ddd5cab7e2b475
yun <loverglory233@gmail.com>
2026-09-04 18:10:10 +0800

    fix: precise 3-min silence check (<50 chars) and zero global lock (v20260904_v2425)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2420';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2420';
+} from './constants.js?v=20260904_v2425';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2425';
 
 export class AuthManager {
   constructor() {
