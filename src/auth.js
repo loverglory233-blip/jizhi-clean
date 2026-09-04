ce20da668ca625251633f770025fc08ff53f0fb6
yun <loverglory233@gmail.com>
2026-09-04 15:46:35 +0800

    feat(vote): separate system vote tally message and remove vote numbers from agent guidance speech

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2234';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2234';
+} from './constants.js?v=20260904_v2235';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2235';
 
 export class AuthManager {
   constructor() {
