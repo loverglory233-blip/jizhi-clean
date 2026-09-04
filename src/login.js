a5552a021e1054b09165932bdde6964a16b1ab26
yun <loverglory233@gmail.com>
2026-09-04 23:52:21 +0800

    fix: 服务端与客户端全链路强化延期时间保护，确保延期绝不被任何旧快照或轮询冲刷覆盖 (v20260904_v2502)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2501";
-import { APP_VERSION } from "./constants.js?v=20260904_v2501";
+import { escapeHtml } from "./utils.js?v=20260904_v2502";
+import { APP_VERSION } from "./constants.js?v=20260904_v2502";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
