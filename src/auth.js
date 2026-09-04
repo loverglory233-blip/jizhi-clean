1b49f2063590fda4118973e981669e397613a5a9
yun <loverglory233@gmail.com>
2026-09-05 00:36:34 +0800

    fix: 重构顶部UI排列，将目标字数移至任务类型后方，参考范文前置并独立实时字数徽标 (v20260904_v2519)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2518';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2518';
+} from './constants.js?v=20260904_v2519';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2519';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2518";
-import { APP_VERSION } from "./constants.js?v=20260904_v2518";
+import { escapeHtml } from "./utils.js?v=20260904_v2519";
+import { APP_VERSION } from "./constants.js?v=20260904_v2519";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
