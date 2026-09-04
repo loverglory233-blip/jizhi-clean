7d19d5e4db22eecbee3eec2114dca6837c082113
yun <loverglory233@gmail.com>
2026-09-05 01:03:28 +0800

    fix: 全阶段彻底清除iframe重载逻辑，建立强一致性协同文档保护架构 (v20260904_v2528)

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2527';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2527';
+import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2528';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2528';
 
 export class CloudSyncEngine {
   constructor(app) {
