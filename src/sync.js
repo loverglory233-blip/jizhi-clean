bea5ef38f872bf756d8ec73f0db01df3fa5a7d09
yun <loverglory233@gmail.com>
2026-09-05 03:09:33 +0800

    build: compile v2646 bundle and clean audit script

--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260905_v2645';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260905_v2645';
+import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260905_v2646';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260905_v2646';
 
 export class CloudSyncEngine {
   constructor(app) {
