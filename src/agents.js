d929b1db3fe6e75ab7a3e122b30b6da6910e542a
yun <loverglory233@gmail.com>
2026-09-05 01:06:40 +0800

    feat: 完善全系统多Pad与数据库长文本一键自动回溯与找回机制 (v20260904_v2530)

--- src/agents.js
+++ src/agents.js
@@ -3,7 +3,7 @@
  * Standard ES Module (ESM)
  */
 
-import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260904_v2529';
+import { AgentProfiles, PresetMessages, STORAGE_KEY_USER } from './constants.js?v=20260904_v2530';
 
 export async function callCozeAgentAPI(botKey, userQuery, currentContext = {}) {
   const profile = AgentProfiles[botKey] || { name: '智能体专家', avatar: '🤖' };
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2529';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2529';
+import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2530';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2530';
 
 export class CloudSyncEngine {
   constructor(app) {
