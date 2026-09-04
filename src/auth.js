d76194f5027c72ff54db9253438709efcb9bf8c6
yun <loverglory233@gmail.com>
2026-09-05 01:21:12 +0800

    fix: 建立全系统只读锁严密屏障，只读/截止状态下彻底禁止触发任何智能体与自动流水线 (v20260904_v2535)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2534';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2534';
+} from './constants.js?v=20260904_v2535';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2535';
 
 export class AuthManager {
   constructor() {
