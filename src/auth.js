e212f6f71a267a8a4b3ccd011f547d90a3664db1
yun <loverglory233@gmail.com>
2026-09-05 00:24:42 +0800

    fix: 彻底移除所有自定义CSS注入，100%还原Etherpad官方原生工具栏与底部布局 (v20260904_v2516)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2515';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2515';
+} from './constants.js?v=20260904_v2516';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2516';
 
 export class AuthManager {
   constructor() {
