010d6c01f5d132b3edfdeda8bd8f44b41d299855
yun <loverglory233@gmail.com>
2026-09-05 07:55:10 +0800

    fix(review): adjust stage 2 first review trigger threshold to 35% of task target word count (v2805)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2804';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice } from './utils.js?v=20260905_v2804';
+} from './constants.js?v=20260905_v2805';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice } from './utils.js?v=20260905_v2805';
 
 export class AuthManager {
   constructor() {
