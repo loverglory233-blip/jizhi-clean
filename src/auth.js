cb839409351de0ebcc8c74e07f22c2079697dafc
yun <loverglory233@gmail.com>
2026-09-04 13:25:17 +0800

    feat: complete 3-min first-action name-calling nudges across all stage milestone buttons (S1 proposal, S1 vote, S1 sign, S2 draft sign, S3 defense confirm, S3 final submit)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2210';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2210';
+} from './constants.js?v=20260904_v2211';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2211';
 
 export class AuthManager {
   constructor() {
