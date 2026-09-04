65573e60a557fb717f6a82304216b749c386ef5a
yun <loverglory233@gmail.com>
2026-09-05 00:06:38 +0800

    fix: 强化Etherpad Node运行环境探测与多级自愈启动通道，秒级消除502并热重载Nginx (v20260904_v2506)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2505';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2505';
+} from './constants.js?v=20260904_v2506';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2506';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2505";
-import { APP_VERSION } from "./constants.js?v=20260904_v2505";
+import { escapeHtml } from "./utils.js?v=20260904_v2506";
+import { APP_VERSION } from "./constants.js?v=20260904_v2506";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
