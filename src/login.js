9a03bf995d44888fb78d6e2f8ce556f47a19c6a3
yun <loverglory233@gmail.com>
2026-09-04 17:59:28 +0800

    fix: remove progress care triggers and strictly keep 3min opening silence prompt (v20260904_v2400)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2395";
-import { APP_VERSION } from "./constants.js?v=20260904_v2395";
+import { escapeHtml } from "./utils.js?v=20260904_v2400";
+import { APP_VERSION } from "./constants.js?v=20260904_v2400";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
