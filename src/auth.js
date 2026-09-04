16b8487321f8b4deaee43c3d1d00da3f63a0b452
yun <loverglory233@gmail.com>
2026-09-05 06:16:10 +0800

    chore: ensure clean realtime task publish and deletion updates in lobby without popups (v2706)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2705';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice } from './utils.js?v=20260905_v2705';
+} from './constants.js?v=20260905_v2706';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice } from './utils.js?v=20260905_v2706';
 
 export class AuthManager {
   constructor() {
