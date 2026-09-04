cb40a45c808e49f935e4b85c77c77487d12f24ed
yun <loverglory233@gmail.com>
2026-09-05 04:53:47 +0800

    chore(audit): verify complete feature availability and release v2675

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2674';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2674';
+} from './constants.js?v=20260905_v2675';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2675';
 
 export class AuthManager {
   constructor() {
