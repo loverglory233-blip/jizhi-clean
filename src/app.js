a6cd5f85837d2a1112f5dcc0a21eeb54218eeb1a
yun <loverglory233@gmail.com>
2026-09-04 17:38:44 +0800

    perf: zero-overhead author scan with dirty check caching and debounced network sync (v20260904_v2360)

--- src/app.js
+++ src/app.js
@@ -13,21 +13,21 @@ import {
   getAgentDisplayName,
   getGenrePromptDescriptor,
   AgentProfiles
-} from "./constants.js?v=20260904_v2355";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isMemberDone, isScopeMatch, showResolutionBlock, safeJsonParse } from "./utils.js?v=20260904_v2355";
-import { callCozeAgentAPI } from "./agents.js?v=20260904_v2355";
-import { AuthManager } from "./auth.js?v=20260904_v2355";
-import { CloudSyncEngine } from "./sync.js?v=20260904_v2355";
-import { renderLoginView } from "./login.js?v=20260904_v2355";
-import { renderTeacherPortal } from "./teacher.js?v=20260904_v2355";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260904_v2355";
+} from "./constants.js?v=20260904_v2360";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isMemberDone, isScopeMatch, showResolutionBlock, safeJsonParse } from "./utils.js?v=20260904_v2360";
+import { callCozeAgentAPI } from "./agents.js?v=20260904_v2360";
+import { AuthManager } from "./auth.js?v=20260904_v2360";
+import { CloudSyncEngine } from "./sync.js?v=20260904_v2360";
+import { renderLoginView } from "./login.js?v=20260904_v2360";
+import { renderTeacherPortal } from "./teacher.js?v=20260904_v2360";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260904_v2360";
 import {
   renderChat,
   renderHeader,
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260904_v2355";
+} from "./editor.js?v=20260904_v2360";
 
 // Make renderChat available on window for sync callbacks and listen to global IME composition
 if (typeof window !== "undefined") {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2355";
-import { APP_VERSION } from "./constants.js?v=20260904_v2355";
+import { escapeHtml } from "./utils.js?v=20260904_v2360";
+import { APP_VERSION } from "./constants.js?v=20260904_v2360";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
