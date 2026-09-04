94fbaf764af05cc565af7b4beaa0483408b2ecbe
yun <loverglory233@gmail.com>
2026-09-05 00:50:32 +0800

    feat: 全面统一所有研讨与引导类静默提醒规则：学生已发言交流则彻底关闭提醒 (v20260904_v2525)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2524';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2524';
+} from './constants.js?v=20260904_v2525';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2525';
 
 export class AuthManager {
   constructor() {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2524';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2524';
+import { InitialState, STORAGE_KEY_TASKS } from './constants.js?v=20260904_v2525';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly } from './utils.js?v=20260904_v2525';
 
 export class CloudSyncEngine {
   constructor(app) {
