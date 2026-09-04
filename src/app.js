9c2277c8a52476363f3d5a2d06e584aa0653f55b
yun <loverglory233@gmail.com>
2026-09-05 01:26:27 +0800

    fix(editor): remove window._hasAttemptedPadSilentAutoHeal from syncPadMetrics (v20260904_v2540)

--- src/app.js
+++ src/app.js
@@ -13,21 +13,21 @@ import {
   getAgentDisplayName,
   getGenrePromptDescriptor,
   AgentProfiles
-} from "./constants.js?v=20260904_v2539";
-import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isMemberDone, isScopeMatch, showResolutionBlock, safeJsonParse } from "./utils.js?v=20260904_v2539";
-import { callCozeAgentAPI } from "./agents.js?v=20260904_v2539";
-import { AuthManager } from "./auth.js?v=20260904_v2539";
-import { CloudSyncEngine } from "./sync.js?v=20260904_v2539";
-import { renderLoginView } from "./login.js?v=20260904_v2539";
-import { renderTeacherPortal } from "./teacher.js?v=20260904_v2539";
-import { renderStudentTaskPortal } from "./student-portal.js?v=20260904_v2539";
+} from "./constants.js?v=20260904_v2540";
+import { downloadFileBlob, escapeHtml, getCaretCharacterOffsetWithin, isTaskExpired, showGlobalBannerNotice, formatStandardDateDash, getUserAllKeys, isSameUser, isUserInMap, getUserFromMap, isMemberDone, isScopeMatch, showResolutionBlock, safeJsonParse } from "./utils.js?v=20260904_v2540";
+import { callCozeAgentAPI } from "./agents.js?v=20260904_v2540";
+import { AuthManager } from "./auth.js?v=20260904_v2540";
+import { CloudSyncEngine } from "./sync.js?v=20260904_v2540";
+import { renderLoginView } from "./login.js?v=20260904_v2540";
+import { renderTeacherPortal } from "./teacher.js?v=20260904_v2540";
+import { renderStudentTaskPortal } from "./student-portal.js?v=20260904_v2540";
 import {
   renderChat,
   renderHeader,
   renderCanvas,
   renderPresencePills,
   renderRemoteCursors
-} from "./editor.js?v=20260904_v2539";
+} from "./editor.js?v=20260904_v2540";
 
 // Make renderChat available on window for sync callbacks and listen to global IME composition
 if (typeof window !== "undefined") {
--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2539';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2539';
+} from './constants.js?v=20260904_v2540';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2540';
 
 export class AuthManager {
   constructor() {
