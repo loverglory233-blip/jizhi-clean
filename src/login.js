204d54b272e2f37a855b7f1c9c18d59b5b500c86
yun <loverglory233@gmail.com>
2026-09-05 07:35:04 +0800

    refactor: 全面彻底清除所有客户端排序选举与房主残留逻辑，实现全员平等状态锁驱动

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260905_v2729";
-import { APP_VERSION } from "./constants.js?v=20260905_v2729";
+import { escapeHtml } from "./utils.js?v=20260905_v2730";
+import { APP_VERSION } from "./constants.js?v=20260905_v2730";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
