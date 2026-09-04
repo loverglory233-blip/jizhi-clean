a25528fb622bcef5a63b3344555663579548bd60
yun <loverglory233@gmail.com>
2026-09-04 17:59:28 +0800

    fix: remove progress care triggers and strictly keep 3min opening silence prompt (v20260904_v2400)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2395';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2395';
+} from './constants.js?v=20260904_v2400';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2400';
 
 export class AuthManager {
   constructor() {
