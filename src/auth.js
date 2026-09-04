fd848dcabca1b153b50ab9c6b4003942eea88e8a
yun <loverglory233@gmail.com>
2026-09-04 16:27:15 +0800

    release: bump version to 20260904_v2250 with clean syntax and localized chat slicing

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2240';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2240';
+} from './constants.js?v=20260904_v2250';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2250';
 
 export class AuthManager {
   constructor() {
