0fe4a180b8e18ae6a2b747cb482e6f6f415bdbc6
yun <loverglory233@gmail.com>
2026-09-04 23:41:51 +0800

    fix: 修复远程同步时任务列表覆盖本地延期截止时间导致工具栏再次被隐藏的问题 (v20260904_v2500)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2499';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2499';
+} from './constants.js?v=20260904_v2500';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2500';
 
 export class AuthManager {
   constructor() {
