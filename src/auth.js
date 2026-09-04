8732ec11c0e4b20f988c59364b27a6887f164444
yun <loverglory233@gmail.com>
2026-09-04 23:15:20 +0800

    fix: restore toolbar and reload etherpad upon deadline extension broadcast (v20260904_v2495)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2490';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2490';
+} from './constants.js?v=20260904_v2495';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2495';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260904_v2490";
-import { APP_VERSION } from "./constants.js?v=20260904_v2490";
+import { escapeHtml } from "./utils.js?v=20260904_v2495";
+import { APP_VERSION } from "./constants.js?v=20260904_v2495";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
