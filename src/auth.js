9aa868627b4e3f9fab85653f2a07bad252145dbd
yun <loverglory233@gmail.com>
2026-09-04 16:02:45 +0800

    fix(contract): upgrade chat extraction and multi-layer JSON/regex parser for robust overview extraction

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2239';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2239';
+} from './constants.js?v=20260904_v2240';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2240';
 
 export class AuthManager {
   constructor() {
