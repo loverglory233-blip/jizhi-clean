84503c0b9447b8868fda9ae8a287f1d135f65073
yun <loverglory233@gmail.com>
2026-09-05 00:14:45 +0800

    fix: 自动纠正宝塔全局npm源为官方最新registry.npmmirror.com，彻底消除镜像404报错 (v20260904_v2511)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260904_v2510';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2510';
+} from './constants.js?v=20260904_v2511';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260904_v2511';
 
 export class AuthManager {
   constructor() {
