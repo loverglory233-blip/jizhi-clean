44fe2332f46ec1d55ba1ad4e67907f018263e48d
yun <loverglory233@gmail.com>
2026-09-05 05:14:29 +0800

    fix(portal): import missing isScopeMatch in student-portal.js (v2682)

--- src/student-portal.js
+++ src/student-portal.js
@@ -8,8 +8,8 @@ import {
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES,
   TASK_GENRE_CONFIGS
-} from "./constants.js?v=20260905_v2681";
-import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice } from "./utils.js?v=20260905_v2681";
+} from "./constants.js?v=20260905_v2682";
+import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch } from "./utils.js?v=20260905_v2682";
 
 /* ==========================================================================
    10. STUDENT TASK PORTAL (CENTRALIZED HUB & COLLABORATION ENTRY)
