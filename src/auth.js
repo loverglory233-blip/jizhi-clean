ce3ea39b5a522c017edf3e85f983b0ded42c02b7
yun <loverglory233@gmail.com>
2026-09-05 05:12:52 +0800

    refactor(chat): strip all artificial dedup heuristics and restore standard chat data flow (v2681)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2680';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2680';
+} from './constants.js?v=20260905_v2681';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch } from './utils.js?v=20260905_v2681';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260905_v2680";
-import { APP_VERSION } from "./constants.js?v=20260905_v2680";
+import { escapeHtml } from "./utils.js?v=20260905_v2681";
+import { APP_VERSION } from "./constants.js?v=20260905_v2681";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
