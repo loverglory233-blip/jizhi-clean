eca15fbfbfd8a8336db82a9b818b923783accba4
yun <loverglory233@gmail.com>
2026-09-05 11:38:30 +0800

    refactor(review): remove student ID initiator election, rely strictly on server distributed atomic lock (v2809)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260905_v2808";
-import { APP_VERSION } from "./constants.js?v=20260905_v2808";
+import { escapeHtml } from "./utils.js?v=20260905_v2809";
+import { APP_VERSION } from "./constants.js?v=20260905_v2809";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/student-portal.js
+++ src/student-portal.js
@@ -8,8 +8,8 @@ import {
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES,
   TASK_GENRE_CONFIGS
-} from "./constants.js?v=20260905_v2808";
-import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch } from "./utils.js?v=20260905_v2808";
+} from "./constants.js?v=20260905_v2809";
+import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch } from "./utils.js?v=20260905_v2809";
 
 /* ==========================================================================
    10. STUDENT TASK PORTAL (CENTRALIZED HUB & COLLABORATION ENTRY)
