addafd5e8c4777d43ba09baaab596429400d33c6
yun <loverglory233@gmail.com>
2026-09-06 01:57:56 +0800

    fix: restore managing and reviewing editor chat messages from overzealous deduplication filter (v2672)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2671";
-import { APP_VERSION } from "./constants.js?v=20260906_v2671";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2672";
+import { APP_VERSION } from "./constants.js?v=20260906_v2672";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/student-portal.js
+++ src/student-portal.js
@@ -8,8 +8,8 @@ import {
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES,
   TASK_GENRE_CONFIGS
-} from "./constants.js?v=20260906_v2671";
-import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch, isSameId } from "./utils.js?v=20260906_v2671";
+} from "./constants.js?v=20260906_v2672";
+import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch, isSameId } from "./utils.js?v=20260906_v2672";
 
 /* ==========================================================================
    10. STUDENT TASK PORTAL (CENTRALIZED HUB & COLLABORATION ENTRY)
