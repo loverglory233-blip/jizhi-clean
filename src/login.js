dd970b3c9b03c438aa5df8da01c47c9b56d2cd07
yun <loverglory233@gmail.com>
2026-09-06 00:28:17 +0800

    feat: apply frozen contribution and dynamic tracking architecture to Stage 3 (v2643)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2642";
-import { APP_VERSION } from "./constants.js?v=20260906_v2642";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2643";
+import { APP_VERSION } from "./constants.js?v=20260906_v2643";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
