9739e93a5069cc7e6a05b9b37b0411b01d1f8547
yun <loverglory233@gmail.com>
2026-09-05 22:50:15 +0800

    fix: v2616 check iframe.src directly for readOnly=true to reliably detect and reload readonly Etherpad sessions

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2615";
-import { APP_VERSION } from "./constants.js?v=20260905_v2615";
+import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2616";
+import { APP_VERSION } from "./constants.js?v=20260905_v2616";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
