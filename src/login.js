eca789f24bf2c493d07cc42976463926e38e434c
yun <loverglory233@gmail.com>
2026-09-05 22:02:47 +0800

    fix: restore isTaskDeadlineExpired declaration in renderStage2Canvas (v2612)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2611";
-import { APP_VERSION } from "./constants.js?v=20260905_v2611";
+import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2612";
+import { APP_VERSION } from "./constants.js?v=20260905_v2612";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
