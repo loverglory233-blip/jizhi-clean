173bc1d61ea6a06269a47fc84b8bc8a4ff5623fd
yun <loverglory233@gmail.com>
2026-09-06 02:08:18 +0800

    fix: restore reviewing editor silence nudge after second review checklist (v2674)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260906_v2673';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2673';
+} from './constants.js?v=20260906_v2674';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2674';
 
 export class AuthManager {
   constructor() {
