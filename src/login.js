a65313edaadcb177f7c0a9eabf2219e11fa9f99f
yun <loverglory233@gmail.com>
2026-09-06 02:56:09 +0800

    fix: eliminate infinite synchronous recursion in Stage 3 committee pipeline (v2691)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2690";
-import { APP_VERSION } from "./constants.js?v=20260906_v2690";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2691";
+import { APP_VERSION } from "./constants.js?v=20260906_v2691";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
