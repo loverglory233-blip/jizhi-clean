51cde2d1a2ddc1f40688890dcf8ba80e7ff6ec21
yun <loverglory233@gmail.com>
2026-09-06 01:23:46 +0800

    fix(etherpad): restore dirtyDB storage engine for rock-solid real-time stability (v2661)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260906_v2660';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2660';
+} from './constants.js?v=20260906_v2661';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2661';
 
 export class AuthManager {
   constructor() {
