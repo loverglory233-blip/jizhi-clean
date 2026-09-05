3f2372808d4224476096b47b49e3742b3a3f74d1
yun <loverglory233@gmail.com>
2026-09-05 13:48:36 +0800

    fix(timer): harden stage agent reminder timers and deduplication across tasks, bump version to v2549

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2548';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2548';
+} from './constants.js?v=20260905_v2549';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2549';
 
 export class AuthManager {
   constructor() {
