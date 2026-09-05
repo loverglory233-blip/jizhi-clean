d6a4b0a33a2831e2ca38aa0e8ffe9cccc497e61b
yun <loverglory233@gmail.com>
2026-09-05 13:12:14 +0800

    build: enforce strictly monotonic increasing build version (20260905_v2546) across all assets

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v1310';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v1310';
+} from './constants.js?v=20260905_v2546';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2546';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml } from "./utils.js?v=20260905_v1310";
-import { APP_VERSION } from "./constants.js?v=20260905_v1310";
+import { escapeHtml } from "./utils.js?v=20260905_v2546";
+import { APP_VERSION } from "./constants.js?v=20260905_v2546";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
