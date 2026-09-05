3020f17ed4062c37fe12892a835e5f0ba87f5815
yun <loverglory233@gmail.com>
2026-09-06 00:38:53 +0800

    fix: restore Etherpad author background highlight colors and prevent unassigned text distorting contribution proportions (v2646)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2645";
-import { APP_VERSION } from "./constants.js?v=20260906_v2645";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2646";
+import { APP_VERSION } from "./constants.js?v=20260906_v2646";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
