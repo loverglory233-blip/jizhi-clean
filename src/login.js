1853ecb16c79f8de9e1587db44dff6cb6ea3dcb4
yun <loverglory233@gmail.com>
2026-09-05 23:24:21 +0800

    fix: v2627 ensure stage2 is strictly writable during stage2 when task is not expired, avoiding false archived readonly lock

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2626";
-import { APP_VERSION } from "./constants.js?v=20260905_v2626";
+import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2627";
+import { APP_VERSION } from "./constants.js?v=20260905_v2627";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
