7765f229139fd354801ba6dc29092fd637c12ab8
yun <loverglory233@gmail.com>
2026-09-05 13:42:50 +0800

    fix(sync): import isSameId in app.js and sync.js, increment version to v2548

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2547';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2547';
+} from './constants.js?v=20260905_v2548';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2548';
 
 export class AuthManager {
   constructor() {
