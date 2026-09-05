24b4bd588afb46de8bdf924b9c432a79db200b57
yun <loverglory233@gmail.com>
2026-09-06 02:12:19 +0800

    feat: complete dynamic 4-item/3-item actionPlan checklist and clean 3m silence nudges (v2677)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260906_v2676';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2676';
+} from './constants.js?v=20260906_v2677';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2677';
 
 export class AuthManager {
   constructor() {
