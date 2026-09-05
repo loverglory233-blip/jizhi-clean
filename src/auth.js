37191a818440026060066aec751ad7bd0ba34d56
yun <loverglory233@gmail.com>
2026-09-05 16:57:28 +0800

    fix(extraction): fix isAnyExtracting check, support direct retry on failure, inform user on readonly mode

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2575';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2575';
+} from './constants.js?v=20260905_v2576';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2576';
 
 export class AuthManager {
   constructor() {
