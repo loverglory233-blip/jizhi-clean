86fa6376c2e04e25494d1106ac59513f07415914
yun <loverglory233@gmail.com>
2026-09-06 01:25:21 +0800

    fix(etherpad): ensure full 777 permissions on var and dirty.db to eliminate editing lock issues (v2662)

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260906_v2661';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2661';
+} from './constants.js?v=20260906_v2662';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260906_v2662';
 
 export class AuthManager {
   constructor() {
--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2661";
-import { APP_VERSION } from "./constants.js?v=20260906_v2661";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2662";
+import { APP_VERSION } from "./constants.js?v=20260906_v2662";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
