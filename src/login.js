410588e8af9024c314a2c7a441f32bd32c0ce326
yun <loverglory233@gmail.com>
2026-09-06 02:09:56 +0800

    feat: assemble actionPlan checklist by combining managing editor consistency diagnosis as item 1 when divergence exists (v2675)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2674";
-import { APP_VERSION } from "./constants.js?v=20260906_v2674";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2675";
+import { APP_VERSION } from "./constants.js?v=20260906_v2675";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/student-portal.js
+++ src/student-portal.js
@@ -8,8 +8,8 @@ import {
   STORAGE_KEY_ANNOUNCEMENTS,
   STORAGE_KEY_CLASSES,
   TASK_GENRE_CONFIGS
-} from "./constants.js?v=20260906_v2674";
-import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch, isSameId } from "./utils.js?v=20260906_v2674";
+} from "./constants.js?v=20260906_v2675";
+import { escapeHtml, isTaskExpired, formatDurationHuman, formatStandardDateDash, showGlobalBannerNotice, isScopeMatch, isSameId } from "./utils.js?v=20260906_v2675";
 
 /* ==========================================================================
    10. STUDENT TASK PORTAL (CENTRALIZED HUB & COLLABORATION ENTRY)
