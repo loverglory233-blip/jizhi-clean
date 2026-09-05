0652c1e7534a41511e80c42c377af134bc86f5a1
yun <loverglory233@gmail.com>
2026-09-06 01:46:58 +0800

    fix: resolve ReferenceError in S2 managing summary generation to ensure seamless pipeline execution (v2668)

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2667";
-import { APP_VERSION } from "./constants.js?v=20260906_v2667";
+import { escapeHtml, isSameId } from "./utils.js?v=20260906_v2668";
+import { APP_VERSION } from "./constants.js?v=20260906_v2668";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
