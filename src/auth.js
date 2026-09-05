269331d86e31cf75ca7f3ca396f05bfcc16892bc
yun <loverglory233@gmail.com>
2026-09-05 18:52:30 +0800

    feat: ensure full coverage of agent analyzing indicator and retry button for vote guidance and stage summaries

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2593';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2593';
+} from './constants.js?v=20260905_v2594';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2594';
 
 export class AuthManager {
   constructor() {
