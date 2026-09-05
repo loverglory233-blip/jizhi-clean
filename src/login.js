89f44c65f5ae205ecc364edc0de22ee37a43b7db
yun <loverglory233@gmail.com>
2026-09-05 23:02:16 +0800

    fix: v2619 remove all container shields, strip body.readonly, enforce active toolbar and innerdocbody cursor:text

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
-import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2618";
-import { APP_VERSION } from "./constants.js?v=20260905_v2618";
+import { escapeHtml, isSameId } from "./utils.js?v=20260905_v2619";
+import { APP_VERSION } from "./constants.js?v=20260905_v2619";
 
 export function renderLoginView(container, authManager, onLoginSuccess) {
   if (authManager && authManager.pullGlobalMeta) {
--- src/sync.js
+++ src/sync.js
@@ -3,8 +3,8 @@
  * Standard ES Module (ESM)
  */
 
-import { InitialState, STORAGE_KEY_TASKS, STORAGE_KEY_ANNOUNCEMENTS } from './constants.js?v=20260905_v2618';
-import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly, filterAndDeduplicateChatLogs, isSameId, normalizeId } from './utils.js?v=20260905_v2618';
+import { InitialState, STORAGE_KEY_TASKS, STORAGE_KEY_ANNOUNCEMENTS } from './constants.js?v=20260905_v2619';
+import { getCaretCharacterOffsetWithin, setCaretPositionWithin, isTaskExpired, showGlobalBannerNotice, showTaskExtendedUnlockModal, isSameUser, getUserAllKeys, getUserFromMap, liftEtherpadReadonly, filterAndDeduplicateChatLogs, isSameId, normalizeId } from './utils.js?v=20260905_v2619';
 
 export class CloudSyncEngine {
   constructor(app) {
