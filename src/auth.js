d7a351ffa4987e98d8e1f67f53bb4ff21ade4051
yun <loverglory233@gmail.com>
2026-09-05 23:11:29 +0800

    fix: v2623 explicitly unbind blockEdit listeners on readonly lifting to guarantee immediate typing

--- src/auth.js
+++ src/auth.js
@@ -14,8 +14,8 @@ import {
   DefaultTasks,
   DefaultAnnouncements,
   DefaultReferencePapers
-} from './constants.js?v=20260905_v2622';
-import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2622';
+} from './constants.js?v=20260905_v2623';
+import { formatExportDateTime, formatDurationHuman, isScopeMatch, showGlobalBannerNotice, isSameId, normalizeId } from './utils.js?v=20260905_v2623';
 
 export class AuthManager {
   constructor() {
